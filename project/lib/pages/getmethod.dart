04e004fb68e222832b06c01c7a7638a807ac5f2b
farihamustafa <113327384+farihamustafa@users.noreply.github.com>
2024-06-02 12:55:44 -0700

    get method

--- project/lib/pages/getmethod.dart
+++ project/lib/pages/getmethod.dart
@@ -1,28 +1,62 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:project/pages/model.dart';
 
-class Viewpage extends StatefulWidget {
-  const Viewpage({super.key});
+class Getmethod extends StatefulWidget {
+  const Getmethod({super.key});
 
   @override
-  State<Viewpage> createState() => _ViewpageState();
+  State<Getmethod> createState() => _GetmethodState();
 }
 
-class _ViewpageState extends State<Viewpage> {
+class _GetmethodState extends State<Getmethod> {
+  List<getlist> Mylist = [];
+  Future<List<getlist>> getlistapi() async {
+    final response =
+        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        Mylist.add(getlist.fromJson(i));
+      }
+      return Mylist;
+    } else
+      return Mylist;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text("List"),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getlistapi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Text("loading");
+                } else
+                  return ListView.builder(
+                    itemCount: Mylist.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Column(
+                          children: [
+                            Text(Mylist[index].name.toString()),
+                            Text(Mylist[index].id.toString()),
+                            Text(Mylist[index].email.toString()),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+              },
+            ),
+          ),
+        ],
       ),
-      body: FloatingActionButton(onPressed: () {
-        fetchusers();
-      }),
     );
-  }
-
-  void fetchusers() {
-    print("fetch users called");
   }
 }
